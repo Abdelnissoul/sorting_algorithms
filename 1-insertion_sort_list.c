c9b5be4912161555ec84b53be00c2759a4b967e3
Abdelnissoul <abdelnissoul205@gmail.com>
2024-02-16 15:51:14 +0000

    solving it using a function helper

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,31 @@
 #include "sort.h"
 
 /**
+ * swap_elems - function helper to swap between elements
+ * @list: Pointer to the head of the list
+ * @elem1: Pointer to the first element
+ * @elem2: Pointer to the second element
+ */
+void swap_elems(listint_t **list, listint_t *elem1, listint_t *elem2)
+{
+    if (elem1 == NULL || elem2 == NULL)
+        return;
+
+    if (elem1->prev != NULL)
+        elem1->prev->next = elem2;
+    else
+        *list = elem2;
+
+    if (elem2->next != NULL)
+        elem2->next->prev = elem1;
+
+    elem1->next = elem2->next;
+    elem2->prev = elem1->prev;
+    elem1->prev = elem2;
+    elem2->next = elem1;
+}
+
+/**
  * insertion_sort_list - Sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm
  * @list: Pointer points to the head.
@@ -10,9 +35,7 @@ void insertion_sort_list(listint_t **list)
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    listint_t *count;
-    listint_t *ptr;
-    listint_t *next;
+    listint_t *count, *ptr, *next;
 
     count = (*list)->next;
 
@@ -23,22 +46,8 @@ void insertion_sort_list(listint_t **list)
 
         while (ptr && ptr->n > count->n)
         {
-            ptr->next = count->next;
-            if (count->next != NULL)
-                count->next->prev = ptr;
-
-            count->next = ptr;
-            count->prev = ptr->prev;
-
-            if (ptr->prev != NULL)
-                ptr->prev->next = count;
-            ptr->prev = count;
-
-            if (count->prev == NULL)
-                *list = count;
-
+            swap_elems(list, ptr, count);
             print_list(*list);
-
             ptr = count->prev;
         }
         count = next;
