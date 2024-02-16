c16f1915fc553f4e32ac86de4de2eb8ddba9c677
Abdelnissoul <abdelnissoul205@gmail.com>
2024-02-16 16:02:26 +0000

    solving it using a function helper

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,28 +1,24 @@
 #include "sort.h"
 
 /**
- * swap_elems - function helper to swap between elements
- * @list: Pointer to the head of the list
- * @elem1: Pointer to the first element
- * @elem2: Pointer to the second element
+ * swap_elems - function helps to swap between elements
+ * @h: A pointer to the head of the doubly-linked list.
+ * @elem1: A pointer to the first element.
+ * @elem2: ptr The second element to swap.
  */
-void swap_elems(listint_t **list, listint_t *elem1, listint_t *elem2)
+void swap_elems(listint_t **h, listint_t **elem1, listint_t *elem2)
 {
-    if (elem1 == NULL || elem2 == NULL)
-        return;
-
-    if (elem1->prev != NULL)
-        elem1->prev->next = elem2;
-    else
-        *list = elem2;
-
-    if (elem2->next != NULL)
-        elem2->next->prev = elem1;
-
-    elem1->next = elem2->next;
-    elem2->prev = elem1->prev;
-    elem1->prev = elem2;
-    elem2->next = elem1;
+	(*elem1)->next = elem2->next;
+	if (elem2->next != NULL)
+		elem2->next->prev = *elem1;
+	elem2->prev = (*elem1)->prev;
+	elem2->next = *elem1;
+	if ((*elem1)->prev != NULL)
+		(*elem1)->prev->next = elem2;
+	else
+		*h = elem2;
+	(*elem1)->prev = elem2;
+	*elem1 = elem2->prev;
 }
 
 /**
@@ -32,24 +28,19 @@ void swap_elems(listint_t **list, listint_t *elem1, listint_t *elem2)
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	listint_t *hseb, *bedel, *cpt;
 
-    listint_t *count, *ptr, *next;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    count = (*list)->next;
-
-    while (count)
-    {
-        ptr = count->prev;
-        next = count->next;
-
-        while (ptr && ptr->n > count->n)
-        {
-            swap_elems(list, ptr, count);
-            print_list(*list);
-            ptr = count->prev;
-        }
-        count = next;
-    }
+	for (hseb = (*list)->next; hseb != NULL; hseb = cpt)
+	{
+		cpt = hseb->next;
+		bedel = hseb->prev;
+		while (bedel != NULL && hseb->n < bedel->n)
+		{
+			swap_elems(list, &bedel, hseb);
+			print_list((const listint_t *)*list);
+		}
+	}
 }
